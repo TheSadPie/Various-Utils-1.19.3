8d7dd59d9e4056ea92f467fe5f81825cff4c41be
Elisa <acrazypie5@gmail.com>
2023-01-17 21:43:10 +0100

    Initial commit (empty).

---
+++ src/main/java/thesadpie/variousutils/VariousUtilsClient.java
@@ -0,0 +1,10 @@
+package thesadpie.variousutils;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VariousUtilsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/thesadpie/variousutils/VariousUtilsDataGenerator.java
@@ -0,0 +1,11 @@
+package thesadpie.variousutils;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VariousUtilsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
